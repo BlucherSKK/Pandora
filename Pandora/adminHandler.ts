8e57df07be7bec79351dc9baadc8e04d25e4a9b9
BlucherSKK <rus.bondarenko2007@gmail.com>
2025-09-08 21:09:33 +0300

    remake logic handling admin command

--- Pandora/adminHandler.ts
+++ Pandora/adminHandler.ts
@@ -22,6 +22,7 @@ import {
     InteractionResponse,  
     User} from 'discord.js';
 import { BruhFn } from './Pandora';
+import fs from 'fs';
 
 export async function message_command_handler(
     msg: Message,
@@ -29,27 +30,57 @@ export async function message_command_handler(
 ): Promise<void> {
     
     if(!(is_moder_message(msg))){return;}
+    if(!(msg.member instanceof GuildMember)){return;}
 
     switch(msg.content){
         case "_get_logs":
-            
+            get_log_file(msg, msg.member, log_file);
+            break;
+        case "_clear_anime_list":
     }
 
 
     
 }
 
+async function msg_reply(
+    rep: string, 
+    del_msg: boolean,
+    msg: Message,
+    del_reply: boolean,
+): Promise<void> {
+    let reply = await msg.reply(rep);
+    if(del_msg){
+        try{
+            msg.delete();
+        } catch {
+            BruhFn.low.logHandle(`Ошибка удоления админской комманды: ${msg.content}`);
+        }
+    }
+    if(del_reply){
+        setTimeout(async () => {
+            try {
+                await reply.delete();
+            } catch (err) {
+                BruhFn.low.logHandle(err);
+            }
+        }, 10000);
+    }
+    
+}
+
 async function get_log_file(message: Message, member: GuildMember, logfilePath: string) {
+    member.send({ 
+        embeds: [new EmbedBuilder()
+            .setTitle("Вот запрошенные логи.")
+            .setColor(BruhFn.COLOR.STD_REQUAST)],
+        files: [logfilePath]
+        });
+}
 
-            if (!(member.permissions.has(PermissionFlagsBits.BanMembers))){ return; }
-            if (message.channel instanceof TextChannel) {message.reply("Жди");}
-
-            member.send({ 
-                embeds: [new EmbedBuilder()
-                    .setTitle("Вот запрошенные логи.")
-                    .setColor(BruhFn.COLOR.STD_REQUAST)],
-                files: [logfilePath]
-                });
+async function clear_anime_list(msg: Message, filePath: string) {
+    await fs.writeFileSync(filePath, "", 'utf-8');
+    msg.reply("Готово");
 }
 
 export function is_moder_message(message: Message): boolean {
